61bc069392acb6adb4343d41d6294098a65cd57a
Aneel Kaushik <v-ankaus@microsoft.com>
2015-05-14 18:18:10 +0530

    1. Data binding

---
+++ app/controllers/login.js
@@ -0,0 +1,79 @@
+xtApp.controller('AccessController',['$scope','$http',function($scope,$http){
+	(function(){
+		"use strict";
+// Login CALL
+$scope.login=function(){
+	var data={};
+	try{
+		if($scope.isSigninValid())
+		{
+				// creating a data object
+				data.username=$scope.loginUserName;
+				data.password=$scope.loginPassword;
+               // get methods to trigger data
+               $http.get('app/data/successLogin.json').success(function(res,status,headers,config){
+               	if(res!= udefined && res.status!= undefined && res.status.indexOf('success')>-1)
+               		console.log(res.records);
+               })
+               .error(function(res,status,headers,config){
+               	console.log(status);
+               });
+           }
+
+       }catch(e){
+
+       	console.log(e.message);
+       }
+   }
+// Sign in Validation
+$scope.isSigninValid = function(){
+	if($scope.loginUserName!=undefined && $scope.loginUserName!= null && $scope.loginPassword!=undefined && $scope.loginPassword!= null)
+		return true;
+	return false;
+}
+
+//SIgnup Calls
+
+$scope.signup = function(){
+   var data = {};
+   try{
+   	if($scope.isSignupValid()){
+   		data.firstname = $scope.firstName;
+   		data.lastname = $scope.LastName;
+   		data.email = $scope.emailAddress;
+   		data.password = $scope.userPassword;
+   		data.password = $scope.userPassword;
+   		data.confirmpwd = $scope.confirmPassword;
+   		data.mobile = $scope.mobileNumber;
+		// post methods to trigger data
+		$http.post('url',data).success(function(res,status,headers,config){
+           
+
+		}).error(function(res,status,headers,config){
+
+		});
+   	}
+
+   } catch(e){
+
+   };
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+})();
+}]);
+
+
+
+
--- app/xtapp.js
+++ app/xtapp.js
@@ -3,13 +3,15 @@
 var xtApp = angular.module('xtApp', ["ui.router"])
     xtApp.config(function($stateProvider, $urlRouterProvider){
       
+
       // For any unmatched url, send to /route1
       $urlRouterProvider.otherwise("/home")
       
       $stateProvider
         .state('home', {
             url: "/home",
-            templateUrl: "app/templates/home.html"
-        })
+            templateUrl: "app/templates/home.html",
+            controller:"AccessController",
+        });
     })
          
